a808b479a926746aa5ac2530a659b76a90c02444
Mattthew Lusty <mattlusty123@gmail.com>
2021-11-04 19:12:17 +0000

    readded images to mind

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,20 +2,26 @@ import React, { Component } from "react";
 // style
 import "../styles/css/Page.css";
 
-// import brave from "../images/bravey.png";
-// import zebramix from "../images/zebby.png";
+import brave from "../images/bravey.png";
+import zebramix from "../images/zebby.png";
 
 class Page extends Component {
   render() {
     return (
       <div className="Page">
         <div className="container">
-          <div className="item item1">{/* <img src={brave} alt="thing" /> */}</div>
-          <div className="item item2">{/* <img src={zebramix} alt="thing" /> */}</div>
-          <div className="item item3">{/* <img src={brave} alt="thing" /> */}</div>
-          <div className="item item4">{/* <img src={zebramix} alt="thing" /> */}</div>
-          <div className="item item3">{/* <img src={brave} alt="thing" /> */}</div>
-          <div className="item item4">{/* <img src={zebramix} alt="thing" /> */}</div>
+          <div className="item item1">
+            <img src={brave} alt="thing" />
+          </div>
+          <div className="item item2">
+            <img src={zebramix} alt="thing" />
+          </div>
+          <div className="item item3">
+            <img src={brave} alt="thing" />
+          </div>
+          <div className="item item4">
+            <img src={zebramix} alt="thing" />
+          </div>
         </div>
       </div>
     );
