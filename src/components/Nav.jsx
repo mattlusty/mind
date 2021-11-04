bdc28170cc88d29a9129e364d65057ab2f99578a
Mattthew Lusty <mattlusty123@gmail.com>
2021-11-04 13:35:20 +0000

    fixed nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,21 +12,23 @@ class Nav extends Component {
   render() {
     return (
       <div className="Nav">
-        <div className="menuToggle icon">
-          <i className="mai-menu"></i>
+        <div className="nav">
+          <div className="menuToggle icon">
+            <i className="mai-menu"></i>
+          </div>
+          {/* <img className="logo" src={brain} alt="brain" /> */}
+          <div className="logoName">iMind</div>
+          <NavSearch />
+          <div className="navMenu">
+            <ul>
+              <li>About</li>
+              <li>Vote</li>
+              <li>Pitch</li>
+              <li>Invest</li>
+            </ul>
+          </div>
+          <NavProfile />
         </div>
-        {/* <img className="logo" src={brain} alt="brain" /> */}
-        <div className="logoName">iMind</div>
-        <NavSearch />
-        <div className="navMenu">
-          <ul>
-            <li>About</li>
-            <li>Vote</li>
-            <li>Pitch</li>
-            <li>Invest</li>
-          </ul>
-        </div>
-        <NavProfile />
       </div>
     );
   }
