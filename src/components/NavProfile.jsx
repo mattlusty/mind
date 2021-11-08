d53dedd0fa815d9f562694148e6628f06b1fe042
Mattthew Lusty <mattlusty123@gmail.com>
2021-11-08 19:22:23 +0000

    added noSelect class

--- src/components/NavProfile.jsx
+++ src/components/NavProfile.jsx
@@ -10,7 +10,7 @@ class NavProfile extends Component {
   };
 
   render() {
-    let classes = `Profile ${this.state.open ? "open" : ""}`;
+    let classes = `Profile ${this.state.open ? "open" : ""} noSelect`;
     return (
       <div
         className={classes}
