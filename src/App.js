34fd81ec4629ca385d18376359e5187244b8f757
Mattthew Lusty <mattlusty123@gmail.com>
2021-11-09 00:27:16 +0000

    added Landing content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from "./components/Nav";
+import Landing from "./components/Landing";
 import Page from "./components/Page";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
       <Page />
     </div>
   );
---
+++ src/components/Landing.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+// style
+import "../styles/css/Landing.css";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="Landing">
+        <div className="one">
+          <p>Every problem has it's solution</p>
+          <p>You draw the dots</p>
+          <p>iMind connects them</p>
+        </div>
+        <div className="two"></div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
