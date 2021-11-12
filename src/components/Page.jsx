3f6e89632bcbe86aa669c71c45ca8bef9cc56f6b
Mattthew Lusty <mattlusty123@gmail.com>
2021-11-12 14:36:54 +0000

    fadded optimised imageges

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 // style
 import "../styles/css/Page.css";
 
-import treeTrunk_2400w from "../images/treeTrunk_2400w.png";
-import treeTrunk_1920w from "../images/treeTrunk_1920w.png";
-import treeTrunk_640w from "../images/treeTrunk_640w.png";
-import treesSnow_2400w from "../images/treesSnow_2400w.jpg";
-import treesSnow_1920w from "../images/treesSnow_1920w.png";
-import treesSnow_640w from "../images/treesSnow_640w.png";
-import treesTall_2400w from "../images/treesTall_2400w.png";
-import treesTall_1920w from "../images/treesTall_1920w.png";
-import treesTall_640w from "../images/treesTall_640w.png";
+import treeTrunk_2500 from "../images/treeTrunk_2500.jpg";
+import treeTrunk_1500 from "../images/treeTrunk_1500.jpg";
+import treeTrunk_500 from "../images/treeTrunk_500.jpg";
+import treesSnow_2500 from "../images/treesSnow_2500.jpg";
+import treesSnow_1500 from "../images/treesSnow_1500.jpg";
+import treesSnow_500 from "../images/treesSnow_500.jpg";
+import treesTall_2500 from "../images/treesTall_2500.jpg";
+import treesTall_1500 from "../images/treesTall_1500.jpg";
+import treesTall_500 from "../images/treesTall_500.jpg";
 
 import brave from "../images/bravey.png";
 import zebramix from "../images/zebby.png";
@@ -22,8 +22,8 @@ class Page extends Component {
         <div className="container">
           <div className="item">
             <img
-              src={treeTrunk_640w}
-              srcSet={`${treeTrunk_2400w} 2400w, ${treeTrunk_1920w} 1920w, ${treeTrunk_640w} 640w`}
+              src={treeTrunk_500}
+              srcSet={`${treeTrunk_2500} 2500w, ${treeTrunk_1500} 1500w, ${treeTrunk_500} 500w`}
               sizes="(max-width: 710px) 100vw, 50%"
               alt="thing"
             />
@@ -42,8 +42,8 @@ class Page extends Component {
           </div>
           <div className="item">
             <img
-              src={treesSnow_640w}
-              srcSet={`${treesSnow_2400w} 2400w, ${treesSnow_1920w} 1920w, ${treesSnow_640w} 640w`}
+              src={treesSnow_500}
+              srcSet={`${treesSnow_2500} 2500w, ${treesSnow_1500} 1500w, ${treesSnow_500} 500w`}
               sizes="(max-width: 710px) 100vw, 25vw"
               alt="thing"
             />
@@ -60,8 +60,8 @@ class Page extends Component {
           </div>
           <div className="item">
             <img
-              src={treesTall_640w}
-              srcSet={`${treesTall_2400w} 2400w, ${treesTall_1920w} 1920w, ${treesTall_640w} 640w`}
+              src={treesTall_500}
+              srcSet={`${treesTall_2500} 2500w, ${treesTall_1500} 1500w, ${treesTall_500} 500w`}
               sizes="(max-width: 710px) 100vw, 25vw"
               alt="thing"
             />
